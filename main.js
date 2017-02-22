b4f915460d1dd8e32efdce9691a9888ce8d18a26
bryanhmiller <bryankalos@hotmail.com>
2017-02-21 19:02:23 -0600

    Added event listener to button in index.html and added the code to make it functional in main.js, also added styling to button in styles.css

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
+var button = document.getElementById("showPlanets");
+button.addEventListener("click", populateDiv);
+
+function populateDiv() {
+	document.getElementById("planetHolder").innerHTML = "<h1>Button Works!</h1>"
+}
+
 console.log("Real Planets");
